38dd9da9a5930d5cf0b460f03feda11e985c74d7
Forhad Hossain <fhossainqr@gmail.com>
2021-09-19 14:48:06 +0300

    create for interview

---
+++ null-vs-undefined.js
@@ -0,0 +1,18 @@
+let Pakhi;
+function add(num1, num2){
+    console.log(num1+num2);
+    return  
+}
+
+function add(num1 , num2 = 10){
+    console.log(num1, num2);
+}
+
+const premik = {name: "Pagol", phone: 454154};
+console.log(premik.gf);
+
+let fun = undefined;
+console.log(fun);
+
+let ages = [2,5,9]
+console.log(ages[11]);
