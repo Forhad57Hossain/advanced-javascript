b75c193075f7cd34a3c9a1888847f022666a1494
Forhad Hossain <fhossainqr@gmail.com>
2021-09-19 14:23:42 +0300

    true-false

---
+++ true-false.js
@@ -0,0 +1,18 @@
+// Falsy
+// false
+// 0
+// ""
+// unenfined
+// null
+// NaN
+//Truthy
+// '0', ' ', [], 
+
+let name = 0;
+console.log(name);
+if(name || name == 0){
+    console.log("condition is true");
+}
+else{
+    console.log("console is false");
+}
